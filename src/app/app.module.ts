e8cbd2bab13f5978f09ef1c260b2ab69de80a7a3
DxSnow <62160409+DxSnow@users.noreply.github.com>
2021-10-14 16:45:34 -0700

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShoppingListGeneratorComponent } from './shopping-list-generator/shopping-list-generator.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingListGeneratorComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shopping-list-generator/shopping-list-generator.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shopping-list-generator',
+  templateUrl: './shopping-list-generator.component.html',
+  styleUrls: ['./shopping-list-generator.component.css']
+})
+export class ShoppingListGeneratorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
