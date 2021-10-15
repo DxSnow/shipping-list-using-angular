5d9d06d3cf8f2bee2a05b200d35f7c091680a9e9
DxSnow <62160409+DxSnow@users.noreply.github.com>
2021-10-15 15:19:25 -0700

    switch string[] to any[] to try anotherway

--- src/app/shopping-list-generator/shopping-list-generator.component.ts
+++ src/app/shopping-list-generator/shopping-list-generator.component.ts
@@ -6,14 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list-generator.component.css']
 })
 export class ShoppingListGeneratorComponent implements OnInit {
+//string version. works great.
+  // item:string = '';
+  // items:string[] = [];
+  // onClick(){
+  //   this.items.push(this.item);
+  //   this.item='';
+  // };
+// object version:
   item:string = '';
-  items:string[] = [];
+  items:any[] = [];//it don't say any[], the default [] is never type.
   onClick(){
-    this.items.push(this.item);
+    this.items.push({"name":this.item});
     this.item='';
   };
 
-  constructor() { }
+
+  constructor() {
+
+  }
 
   ngOnInit(): void {
   }
